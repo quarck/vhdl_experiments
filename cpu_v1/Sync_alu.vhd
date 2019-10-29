9099c65c3621155d3d4b33417824a4c41096c8a4
Sergey Parshin <quarck@gmail.com>
2019-10-29 23:05:08 +0000

    ALU multimplication - v1 working for MUL and IMUL

--- cpu_v1/Sync_alu.vhd
+++ cpu_v1/Sync_alu.vhd
@@ -7,79 +7,152 @@ use work.opcodes.all;
 use work.types.all;
 
 entity sync_ALU is
-    generic (
-        bits    : integer := 8
-    );
-    port
-    (
-        clk_i                   : std_logic;
-        operation               : in std_logic_vector(3 downto 0);
-        regfile_read_port_a     : in std_logic_vector(nbits-1 downto 0);
-        regfile_read_port_b     : in std_logic_vector(nbits-1 downto 0);
-        direct_arg_port_a       : in std_logic_vector(nbits-1 downto 0);
-        direct_arg_port_b       : in std_logic_vector(nbits-1 downto 0);
-        a_val_select            : in ALU_arg_select;
-        b_val_select            : in ALU_arg_select;
-        right_arg               : in std_logic_vector(nbits-1 downto 0);
-        result                  : out std_logic_vector(nbits-1 downto 0);
-        flags                   : out ALU_flags
-    );
+	generic (
+		nbits	: integer := 8
+	);
+	port
+	(
+		clk_i					: std_logic;
+		operation_i				: in std_logic_vector(3 downto 0);
+		left_arg_high_i		: in std_logic_vector(nbits-1 downto 0);
+		left_arg_low_i			: in std_logic_vector(nbits-1 downto 0);
+		right_arg_i				: in std_logic_vector(nbits-1 downto 0);
+		result_high_o			: out std_logic_vector(nbits-1 downto 0);
+		result_low_o			: out std_logic_vector(nbits-1 downto 0);
+		flags_o					: out ALU_flags;
+		alu_start_i				: in std_logic;
+		alu_ready_o				: out std_logic
+	);
 end sync_ALU;
 
 
-architecture behaviour of sync_ALU is   
-    constant all_zeroes : std_logic_vector(nbits-1 downto 0) := (others => '0');
-    constant all_ones : std_logic_vector(nbits-1 downto 0) := (others => '1');
-    constant one : std_logic_vector(nbits-1 downto 0) := (0 => '1', others => '0');
-    constant minus_one : std_logic_vector(nbits-1 downto 0) := (0 => '0', others => '1');
-    
-    signal left_arg : std_logic_vector (nbits-1 downto 0);
-    signal left_arg_high : std_logic_vector (nbits-1 downto 0); -- only for division 
-    signal right_arg : std_logic_vector (nbits-1 downto 0);
-    
+architecture behaviour of sync_ALU is	
+	constant all_zeroes : std_logic_vector(nbits-1 downto 0) := (others => '0');
+	constant all_ones : std_logic_vector(nbits-1 downto 0) := (others => '1');
+	constant one : std_logic_vector(nbits-1 downto 0) := (0 => '1', others => '0');
+	constant minus_one : std_logic_vector(nbits-1 downto 0) := (0 => '0', others => '1');
+
+	type sync_alu_state is (
+		IDLE,
+		ALU_MUL_START, 
+		ALU_MUL_FINISH, 
+		ALU_IMUL_START,
+		ALU_IMUL_FINISH, 
+
+		ALU_MUL_COMPUTE,
+		
+		ALU_DIV_START, 
+		ALU_DIV_FINISH, 
+
+		ALU_IDIV_START,
+		ALU_IDIV_FINISH
+	);
+	
+	signal state : sync_alu_state := IDLE;
+
+
 begin
 
-    with a_val_select select
-        left_arg <= regfile_read_port_a when reg_port,
-                    direct_arg_port_a when others;
+	alu_ready_o <= '1' when state = IDLE else '0'; 
+					
+	process (clk_i)
+		variable m_mask		 	: std_logic_vector(nbits*2-1 downto 0);
+		variable m_acc 			: std_logic_vector(nbits*2-1 downto 0);
+		variable m_left 		: std_logic_vector(nbits*2-1 downto 0);
+		variable m_right 		: std_logic_vector(nbits-1 downto 0);
+		
+		variable im_l_sgn		: std_logic;
+		variable im_r_sgn		: std_logic;		
+	begin
+	
+		if rising_edge(clk_i)
+		then 
+			case state is 
+				when IDLE => 
+					if alu_start_i = '1' then 
+						case operation_i is 
+							when ALU_MUL =>  state <= ALU_MUL_START;
+							when ALU_IMUL => state <= ALU_IMUL_START;
+							when ALU_DIV =>  state <= ALU_DIV_START;
+							when ALU_IDIV => state <= ALU_IDIV_START;
+							when others 	=> state <= IDLE;
+						end case;
+					end if;
 
-    with b_val_select select
-        right_arg <= regfile_read_port_b when reg_port,
-                    direct_arg_port_b when others;
-                    
-    process (left_arg, right_arg, operation, carry_in)
-        variable temp : std_logic_vector(nbits downto 0);
-        variable mask : std_logic_vector(nbits-1 downto 0);
-        variable left_as_uint: integer;
-        variable right_as_uint: integer;
-    begin
-    
-        flags.overflow <= '0';
-        
-        left_as_uint := to_integer(unsigned(left_arg));
-        right_as_uint := to_integer(unsigned(right_arg));
+				when ALU_MUL_START => 
+					m_acc := all_zeroes & all_zeroes; 
+					m_left := all_zeroes & left_arg_low_i;
+					m_right := right_arg_i;
+					state <= ALU_MUL_COMPUTE;
 
-        result2 <= all_zeroes; -- default value 
+				when ALU_IMUL_START => 
+					m_acc := all_zeroes & all_zeroes; 
+					
+					im_l_sgn := left_arg_low_i(nbits-1);
+					im_r_sgn := right_arg_i(nbits-1);
+					
+					if im_l_sgn = '1' 
+					then 
+						-- negative number 
+						m_left := not (all_ones & left_arg_low_i) + 1;
+					else 
+						m_left := all_zeroes & left_arg_low_i;
+					end if;
 
-        case operation is
-            when ALU_MUL =>
-                temp := all_zeroes;
+					if im_r_sgn = '1' 
+					then 
+						-- negative number 
+						m_right := not right_arg_i + 1;
+					else 
+						m_right := right_arg_i;
+					end if;
+					state <= ALU_MUL_COMPUTE;
+				
+				when ALU_MUL_COMPUTE => 
+					
+					for i in 0 to nbits-1 
+					loop 
+						if m_right(i) /= '0' 
+						then 
+							m_mask := all_ones & all_ones;
+						else 
+							m_mask := all_zeroes & all_zeroes;
+						end if;
 
-            when others =>
-                temp := all_zeroes;
-        end case;
+						m_acc := m_acc + (m_left and m_mask);
+						m_left := m_left(2*nbits-2 downto 0) & '0';
+					end loop;
+					
+					if im_l_sgn = '0' and im_r_sgn = '0' then 
+						state <= ALU_MUL_FINISH;
+					else 
+						state <= ALU_IMUL_FINISH;
+					end if;
+				
+				when ALU_MUL_FINISH => 
+					result_high_o <= m_acc(2*nbits-1 downto nbits);
+					result_low_o <= m_acc(nbits-1 downto 0);
+					state <= IDLE;
+				
+				when ALU_IMUL_FINISH => 
+					 if (im_l_sgn xor im_r_sgn) = '1' 
+					 then 
+						m_acc := not m_acc + 1;
+					end if;
+					result_high_o <= m_acc(2*nbits-1 downto nbits);
+					result_low_o <= m_acc(nbits-1 downto 0);
+					state <= IDLE;
 
-        if temp(nbits-1 downto 0) = all_zeroes then 
-            flags.zero <= '1';
-        else 
-            flags.zero <= '0';
-        end if;     
-        
-        flags.carry_out <= temp(nbits);
-        flags.negative <= temp(nbits-1);
-
-        result <= temp(nbits-1 downto 0);
-        
-    end process;
-    
+				when ALU_DIV_START => 
+				
+				when ALU_IDIV_START => 
+				
+				when ALU_DIV_FINISH =>
+				
+				when ALU_IDIV_FINISH =>
+			end case;
+		end if;
+	
+	end process;
+	
 end behaviour;
---
+++ cpu_v1/TB_sync_alu.vhd
@@ -0,0 +1,116 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+use work.opcodes.all;
+use work.types.all;
+
+ENTITY TB_sync_alu IS
+END TB_sync_alu;
+
+ARCHITECTURE behavior OF TB_sync_alu IS 
+
+	component sync_ALU is
+		generic (
+			nbits	: integer := 8
+		);
+		port
+		(
+			clk_i					: std_logic;
+			operation_i				: in std_logic_vector(3 downto 0);
+			left_arg_high_i			: in std_logic_vector(nbits-1 downto 0);
+			left_arg_low_i			: in std_logic_vector(nbits-1 downto 0);
+			right_arg_i				: in std_logic_vector(nbits-1 downto 0);
+			result_high_o			: out std_logic_vector(nbits-1 downto 0);
+			result_low_o			: out std_logic_vector(nbits-1 downto 0);
+			flags_o					: out ALU_flags;
+			alu_start_i				: in std_logic;
+			alu_ready_o				: out std_logic
+		);
+	end component;
+
+   -- Clock period definitions
+	constant clk_period : time := 10 ns; 
+
+    signal clk						: std_logic;
+	signal operation				: std_logic_vector(3 downto 0);
+	signal left_arg_high			: std_logic_vector(7 downto 0);
+	signal left_arg_low				: std_logic_vector(7 downto 0);
+	signal right_arg				: std_logic_vector(7 downto 0);
+	signal result_high				: std_logic_vector(7 downto 0);
+	signal result_low				: std_logic_vector(7 downto 0);
+	signal flags					: ALU_flags;
+	signal alu_start				: std_logic;
+	signal alu_ready				: std_logic;
+
+begin
+
+  -- Component Instantiation
+    uut: sync_ALU port map(
+		clk_i					=> clk,
+		operation_i				=> operation,
+		left_arg_high_i			=> left_arg_high,
+		left_arg_low_i			=> left_arg_low,
+		right_arg_i				=> right_arg,
+		result_high_o			=> result_high,
+		result_low_o			=> result_low,
+		flags_o					=> flags,
+		alu_start_i				=> alu_start,
+		alu_ready_o				=> alu_ready
+	);
+
+clock_process: 
+	process -- clock generator process 
+	begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+	end process;
+
+tb:
+	process
+   begin     
+      operation <= ALU_MUL;
+		left_arg_high <= x"00";
+		left_arg_low <= x"CA";
+		right_arg <= x"23";
+		alu_start <= '1';
+		
+		wait for clk_period;
+		alu_start <= '0';
+		
+      wait for clk_period * 4;  -- expected result: 1B 9E
+		 
+		 operation <= ALU_IMUL;
+		alu_start <= '1';
+		
+		wait for clk_period;
+		alu_start <= '0';
+
+       wait for clk_period * 4; -- expected result: F8 9E
+
+		left_arg_low <= x"23";
+		right_arg <= x"CA";
+
+		alu_start <= '1';
+		
+		wait for clk_period;
+		alu_start <= '0';
+
+       wait for clk_period * 4; -- expected result: F8 9E
+
+
+		left_arg_low <= x"FF";
+		right_arg <= x"FF";
+		alu_start <= '1';
+		
+		wait for clk_period;
+		alu_start <= '0';
+		
+      wait for clk_period * 4;  -- expected result: 00 01 (thatx minus one times minus one)
+
+
+       wait; -- will wait forever
+    end process tb;
+end;
