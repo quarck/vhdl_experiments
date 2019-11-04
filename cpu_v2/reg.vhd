df05fd8da8312f9d193554270f8fd33e3430b752
Sergey Parshin <quarck@gmail.com>
2019-11-04 00:46:19 +0000

    Regfile impl

---
+++ cpu_v2/reg.vhd
@@ -0,0 +1,26 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity reg is
+	generic (
+		nbits	: integer := 8
+	);
+	port (
+		clk_i		: in std_logic;
+		rst_i		: in std_logic;
+		we_i		: in std_logic;
+		data_o		: out std_logic_vector(nbits-1 downto 0);
+		data_i 		: in std_logic_vector(nbits-1 downto 0)
+	);
+end reg;
+
+architecture rtl of reg is
+	signal data : std_logic_vector(nbits-1 downto 0);
+begin
+
+	data <= (others => '0') when rst_i = '1' 
+			else data_i when rising_edge(clk_i) and we_i = '1';
+
+	data_o <= data; -- always - async
+end rtl;
+
--- cpu_v2/regfile.vhd
+++ cpu_v2/regfile.vhd
@@ -8,35 +8,74 @@ entity regfile is
         bits    : integer := 8
     );
     port ( 
-        clk_i                   : in std_logic;
-        rst_i                   : in std_logic;
-        read_select_a_i         : in std_logic_vector(nreg-1 downto 0); 
-        read_select_b_i         : in std_logic_vector(nreg-1 downto 0); 
-        
-        write_select_i          : in std_logic_vector(nreg-1 downto 0);
-        write_enable_i          : in std_logic;
-        
-        port_a_data_read_o      : out std_logic_vector (bits-1 downto 0); 
-        port_b_data_read_o      : out std_logic_vector (bits-1 downto 0); 
+		  -- reads are async, writes are on rising_edge 
+		clk_i			: in std_logic; 
+		rst_i			: in std_logic;
 
-        write_data_i            : in  std_logic_vector (bits-1 downto 0)
+		sel_a_i			: in std_logic_vector(nreg-1 downto 0); 
+		sel_b_i			: in std_logic_vector(nreg-1 downto 0); 		  
+		sel_w_i			: in std_logic_vector(nreg-1 downto 1); 	-- wide port sel
+	
+		wr_sel_i		: in std_logic_vector(nreg-1 downto 0);	-- write port sel
+		we_i			: in std_logic;
+
+		data_a_o		: out std_logic_vector (bits-1 downto 0); 
+		data_b_o		: out std_logic_vector (bits-1 downto 0); 
+		data_w_o		: out std_logic_vector (2*bits-1 downto 0); 
+		data_i			: in  std_logic_vector (bits-1 downto 0)
     );
 end regfile;
 
 architecture rtl of regfile is
-    type regfile_type is array (2**nreg-1 downto 0) of std_logic_vector(bits-1 downto 0);
-    signal regfile : regfile_type := (others => (others => '0'));
+
+	component reg is
+		generic (
+			nbits	: integer := 8
+		);
+		port (
+			clk_i		: in std_logic;
+			rst_i		: in std_logic;
+			we_i		: in std_logic;
+			data_o		: out std_logic_vector(nbits-1 downto 0);
+			data_i 		: in std_logic_vector(nbits-1 downto 0)
+		);
+	end component;
+
+	signal write_enable_vec: std_logic_vector(2**nreg-1 downto 0);
+
+    type data_array_type is array (2**nreg-1 downto 0) of std_logic_vector(bits-1 downto 0);
+    signal data_out_vec : data_array_type;
+	
+	signal wide_sel_low : std_logic_vector(nreg-1 downto 0);
+	signal wide_sel_high : std_logic_vector(nreg-1 downto 0);
+	
 begin
-    port_a_data_read_o <= regfile(to_integer(unsigned(read_select_a_i)));
-    port_b_data_read_o <= regfile(to_integer(unsigned(read_select_b_i)));
-    
-write_process: 
-	process (clk_i, write_enable_i)
-	begin
-		if rising_edge(clk_i) and write_enable_i = '1'
-		then 
-			regfile(to_integer(unsigned(write_select_i))) <= write_data_i;
-		end if;
-	end process;    
+
+gen_registers: 
+	for i in 0 to 2**nreg-1 
+	generate 
+		write_enable_vec(i) <= we_i when to_integer(unsigned(wr_sel_i)) = i 
+							else '0';
+
+		r: reg port map(
+			clk_i		=> clk_i, 
+			rst_i		=> rst_i,
+			we_i		=> write_enable_vec(i),
+			data_o		=> data_out_vec(i),
+			data_i 		=> data_i
+		);	
+	end generate gen_registers;
+
+	-- output multiplexers 
+	data_a_o <= data_out_vec(to_integer(unsigned(sel_a_i)));
+	data_b_o <= data_out_vec(to_integer(unsigned(sel_b_i)));
+
+	wide_sel_low <= sel_w_i & '0';
+	wide_sel_high <= sel_w_i & '1';
+	
+	data_w_o <=
+		data_out_vec(to_integer(unsigned(wide_sel_high))) 
+		& data_out_vec(to_integer(unsigned(wide_sel_low)));
+
 end rtl;
 
